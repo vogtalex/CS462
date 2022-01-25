f0b21ccc362cda0939f4ea3215bb6dbedd015464
vogtalex <vogtalex@oregonstate.edu>
2022-01-25 14:35:37 -0800

    Added sample program and tests

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""
+Calculator app using circle ci.
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
---
+++ tests.py
@@ -0,0 +1,18 @@
+"""
+Tests for calc app.
+"""
+import calculator
+
+
+class TestCalculatorApp:
+    def test_add(self):
+        assert 5 == calculator.add(3, 2)
+
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10, 5)
+
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
+
+    def test_divide(self):
+        assert 2 == calculator.divide(4, 2)
